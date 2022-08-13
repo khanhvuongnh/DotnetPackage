82a5772983c88e87cfe622436a7c35ae54a6833c
(SSB-61B9) Vuong.Nguyen <vuong.nguyen@ssbshoes.com>
2022-08-13 12:12:06 +0700

    API.Helpers.Utilities@6.0.6

--- API.Helpers.Utilities/PaginationUtility.cs
+++ API.Helpers.Utilities/PaginationUtility.cs
@@ -6,10 +6,10 @@ namespace API.Helpers.Utilities
         public PaginationResult Pagination { get; set; }
         public List<T> Result { get; set; }
 
-        public PaginationUtility(List<T> items, int count, int pageNumber, int pageSize, int skip)
+        public PaginationUtility(List<T> items, int count, int pageNumber, int pageSize, int skip, bool isPaging)
         {
             Result = items;
-            Pagination = PaginationResult.Create(count, pageNumber, pageSize, skip);
+            Pagination = PaginationResult.Create(count, pageNumber, pageSize, skip, isPaging);
         }
 
         public static async Task<PaginationUtility<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize = 10, bool isPaging = true)
@@ -17,7 +17,7 @@ namespace API.Helpers.Utilities
             var count = await source.CountAsync();
             var skip = (pageNumber - 1) * pageSize;
             var items = isPaging ? await source.Skip(skip).Take(pageSize).ToListAsync() : await source.ToListAsync();
-            return new PaginationUtility<T>(items, count, pageNumber, pageSize, skip);
+            return new PaginationUtility<T>(items, count, pageNumber, pageSize, skip, isPaging);
         }
 
         public static PaginationUtility<T> Create(List<T> source, int pageNumber, int pageSize = 10, bool isPaging = true)
@@ -25,7 +25,7 @@ namespace API.Helpers.Utilities
             var count = source.Count();
             var skip = (pageNumber - 1) * pageSize;
             var items = isPaging ? source.Skip(skip).Take(pageSize).ToList() : source.ToList();
-            return new PaginationUtility<T>(items, count, pageNumber, pageSize, skip);
+            return new PaginationUtility<T>(items, count, pageNumber, pageSize, skip, isPaging);
         }
 
         public class PaginationResult
@@ -35,19 +35,21 @@ namespace API.Helpers.Utilities
             public int PageNumber { get; set; }
             public int PageSize { get; set; }
             public int Skip { get; set; }
+            public bool IsPaging { get; set; }
 
-            public PaginationResult(int count, int pageNumber, int pageSize, int skip)
+            public PaginationResult(int count, int pageNumber, int pageSize, int skip, bool isPaging)
             {
                 TotalCount = count;
                 TotalPage = (int)Math.Ceiling(TotalCount / (double)pageSize);
                 PageNumber = pageNumber;
                 PageSize = pageSize;
                 Skip = skip;
+                IsPaging = isPaging;
             }
 
-            public static PaginationResult Create(int count, int pageNumber, int pageSize, int skip)
+            public static PaginationResult Create(int count, int pageNumber, int pageSize, int skip, bool isPaging)
             {
-                return new PaginationResult(count, pageNumber, pageSize, skip);
+                return new PaginationResult(count, pageNumber, pageSize, skip, isPaging);
             }
         }
     }
@@ -62,5 +64,6 @@ namespace API.Helpers.Utilities
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
+        public bool IsPaging { get; set; } = true;
     }
 }
